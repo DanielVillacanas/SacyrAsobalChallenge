3a8b07aabdacdceeb713690b8a5f5e2cc14827ed
Daniel Villacañas <dasvilla95@gmail.com>
2022-02-26 00:44:56 +0100

    WIP DANI

--- server/routes/Products/products.js
+++ server/routes/Products/products.js
@@ -2,7 +2,7 @@ const Product = require("../../models/Product.model");
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  const team = req.query;
+  const team = req.query.team;
   if (!team) throw console.error("Missing data");
   Product.find({ owner: team }, { owner: 1, price: 1, img_url: 1, name: 1 })
     .then((products) =>
@@ -11,17 +11,6 @@ router.get("/", (req, res) => {
         : res.status(200).json(products)
     )
     .catch((err) => res.status(400).json({ message: "Error retrieving products", err }));
-});
-router.get("/test", (req, res) => {
-  //   const team = req.params.team;
-  //   if (!team) throw console.error("Missing data");
-  //   Product.find({ owner: team }, { owner: 1, price: 1, img_url: 1, name: 1 })
-  //     .then((products) =>
-  //       products.length < 0
-  //         ? res.status(404).json({ message: "No products" })
-  //         : res.status(200).json(products)
-  //     )
-  //     .catch((err) => res.status(400).json({ message: "Error retrieving products", err }));
 });
 
 module.exports = router;
