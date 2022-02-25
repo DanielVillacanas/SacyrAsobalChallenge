7fe7e2e737f7652405dc3f55c84a5392726eb66f
Daniel Villacañas <dasvilla95@gmail.com>
2022-02-26 00:59:30 +0100

    Products Done

--- server/routes/Products/products.js
+++ server/routes/Products/products.js
@@ -4,7 +4,7 @@ const router = require("express").Router();
 router.get("/", (req, res) => {
   const team = req.query.team;
   if (!team) throw console.error("Missing data");
-  Product.find({ owner: team }, { owner: 1, price: 1, img_url: 1, name: 1 })
+  Product.find({ owner: { $in: team } }, { owner: 1, price: 1, img_url: 1, name: 1 })
     .then((products) =>
       products.length < 0
         ? res.status(404).json({ message: "No products" })
