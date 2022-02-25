f244d417aa7af11dc04a5b4c0caa4c01ea0fe792
Daniel Villacañas <dasvilla95@gmail.com>
2022-02-25 22:33:53 +0100

    WIP

--- server/app.js
+++ server/app.js
@@ -10,8 +10,8 @@ const express = require("express");
 const app = express();
 
 require("./config")(app);
-require("./routes/index.routes")(app);
 require("./config/session.config")(app);
+require("./routes/index.routes")(app);
 require("./error-handling")(app);
 
 module.exports = app;
--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,7 @@
 // ℹ️ package responsible to make the connection with mongodb
 // https://www.npmjs.com/package/mongoose
 const mongoose = require("mongoose");
-console.log(process.env.MONGODB_URI);
+
 const MONGO_URI = process.env.MONGODB_URI;
 
 mongoose
