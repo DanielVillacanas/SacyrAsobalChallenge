02eca186731e5069b954893cd1c5b3142ab164f3
alvaro <alvaro.teran97@gmail.com>
2022-02-26 14:21:25 +0100

    Filter Fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,31 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import { useState, useEffect } from 'react'
-import Login from './Components/Auth/Login/Login'
-import SignUp from './Components/Auth/SignUp/SignUp'
-import Home from './Components/Home/Home'
-import Merchandaising from './Components/Shop/Merchandaising/Merchandaising'
-import MerchandaisingDetails from './Components/Shop/Merchandaising/MerchandaisingDetails'
-import PaymentTickets from './Components/Shop/PaymentTickets/PaymentTickets'
-import Calendar from './Components/Calendar/Calendar'
-import ShoppingCart from './Components/Shop/ShoppingCart/ShoppingCart'
-import { CartProvider } from './Components/Context/CartContext'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Login from "./Components/Auth/Login/Login";
+import SignUp from "./Components/Auth/SignUp/SignUp";
+import Home from "./Components/Home/Home";
+import Merchandaising from "./Components/Shop/Merchandaising/Merchandaising";
+import MerchandaisingDetails from "./Components/Shop/Merchandaising/MerchandaisingDetails";
+import PaymentTickets from "./Components/Shop/PaymentTickets/PaymentTickets";
+import Tickets from "./Components/Shop/Tickets/Tickets";
+import Calendar from "./Components/Calendar/Calendar";
+import ShoppingCart from "./Components/Shop/ShoppingCart/ShoppingCart";
+import { CartProvider } from "./Components/Context/CartContext";
 
 function App() {
-  const [shoppingCart, setShoppingCart] = useState([])
-  const [countCart, setCountCart] = useState(0)
+  const [shoppingCart, setShoppingCart] = useState([]);
+  const [countCart, setCountCart] = useState(0);
 
   const addProduct = (prod) => {
-    setShoppingCart([...shoppingCart, prod])
-  }
+    setShoppingCart([...shoppingCart, prod]);
+  };
 
-  const countItems = () => setCountCart(shoppingCart.length)
+  const countItems = () => setCountCart(shoppingCart.length);
 
   useEffect(() => {
-    countItems()
+    countItems();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [shoppingCart])
+  }, [shoppingCart]);
 
   return (
     <CartProvider value={{ addProduct, shoppingCart, setCountCart }}>
@@ -44,12 +45,12 @@ function App() {
           path="/merchandaising/:id"
           element={<MerchandaisingDetails props={{ countCart }} />}
         ></Route>
-        <Route path="/tickets" element={<Login />}></Route>
+        <Route path="/tickets" element={<Tickets />}></Route>
         <Route path="/payout-tickets" element={<PaymentTickets />}></Route>
         <Route path="/shopping-cart" element={<ShoppingCart />}></Route>
       </Routes>
     </CartProvider>
-  )
+  );
 }
 
-export default App
+export default App;
